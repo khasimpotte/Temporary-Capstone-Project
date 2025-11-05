b7f61d8e0ed1712eac606a63248bedf2a305561d
khasimpotte <your_github_email@example.com>
2025-11-06 02:02:08 +0530

    first commit

--- client/src/AuthProvider.jsx
+++ client/src/AuthProvider.jsx
@@ -4,9 +4,9 @@ import axios from "axios";
 export const AuthContext = createContext({
   isLogged: false,
   checking: true,
-  //reload: () => {},
-  //logout: () => {},
-  login: () => {},
+  reload: () => { },
+  logout: () => { },
+  login: () => { },
 });
 
 export function AuthProvider({ children }) {
@@ -17,28 +17,27 @@ export function AuthProvider({ children }) {
     window.location.href = "http://localhost:3001/auth/login";
   }, []);
 
-  //   const logout = useCallback(async () => {
-  //     await axios.get("http://localhost:3001/auth/logout", {
-  //       withCredentials: true,
-  //     });
-  //     setIsLogged(false);
-  //   }, []);
+  const logout = useCallback(async () => {
+    await axios.get("http://localhost:3001/auth/logout", {
+      withCredentials: true,
+    });
+    setIsLogged(false);
+  }, []);
 
-  // const reload = useCallback(async () => {
-  //     setChecking(true);
-  //     const r = await axios.get('http://localhost:3001/auth/status', {
-  //         withCredentials: true
-  //     });
-  //     setIsLogged(Boolean(r.data.authenticated));
+  const reload = useCallback(async () => {
+    setChecking(true);
+    const r = await axios.get('http://localhost:3001/auth/status', {
+      withCredentials: true
+    });
+    setIsLogged(Boolean(r.data.authenticated));
+  }, [])
 
-  // }, [])
-
-  // useEffect(() => {
-  //     reload();
-  // }, [reload])
+  useEffect(() => {
+    reload();
+  }, [reload])
 
   return (
-    <AuthContext.Provider value={{ isLogged, checking, login }}>
+    <AuthContext.Provider value={{ isLogged, checking, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -6,32 +6,67 @@ import {
   CardContent,
   Grid,
 } from "@mui/material";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "./AuthProvider";
+import axios from "axios";
 
 export default function Home() {
   const { isLogged } = useContext(AuthContext);
+  const [incidents, setIncidents] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      if (isLogged) {
+        const incidentList = await axios.get('http://localhost:3001/api/incidents',
+          { withCredentials: true }
+        );
+        setIncidents(incidentList.data.result);
+      }
+    }
+    fetchData();
+  }, [isLogged])
+
 
   return (
     <>
-      {isLogged ? (
-        <Card sx={{ width: 300 }}>
-          <CardContent>
-            <Typography>Incident #: 1</Typography>
-            <Typography>Description: </Typography>
-            <Typography>State: </Typography>
-            <Typography>Priority: </Typography>
-            <Button variant="contained" color="success">
-              Edit
-            </Button>
-            <Button variant="contained" color="error">
-              Delete
-            </Button>
-          </CardContent>
-        </Card>
+      {isLogged && incidents ? (
+
+        <Stack spacing={5}>
+          <Typography variant="h5">Incident Records:</Typography>
+
+          <Grid container spacing={5} justifyContent={"space-around"}>
+
+            {incidents.map((inc, index) => {
+              return (
+                <Grid key={inc.sys_id}>
+                  <Card sx={{ width: 300, height: 200 }}>
+                    <CardContent>
+                      <Typography variant="h6">Incident : {inc.number}</Typography>
+                      <Typography variant="body2">Description:{inc.short_description} </Typography>
+                      <Typography variant="body2">State:{inc.state} </Typography>
+                      <Typography variant="body2">Priority:{inc.priority} </Typography>
+                      <Button sx={{ mb: 1 }} variant="contained" color="success">
+                        Edit
+                      </Button>
+                      <Button sx={{ mt: 1, mx: 1 }} variant="contained" color="error">
+                        Delete
+                      </Button>
+                    </CardContent>
+                  </Card>
+
+
+                </Grid>
+              );
+            })}
+
+          </Grid>
+
+        </Stack >
+
       ) : (
         <Typography>Please log in</Typography>
-      )}
+      )
+      }
     </>
   );
 }
